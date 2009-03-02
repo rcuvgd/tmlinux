bc0df779d21f3fd3525994a7e654bd6d88de7b81
camelguo <camelguo@c9ba4982-c440-0410-8649-23ceed688df0>
2009-03-02 12:06:23 +0000

    remove the include/lib files which is installed by uClibc

--- sysroots/pnx1500_ttisim_uclibc/sysroot/usr/include/asm/resource.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ASM_TRIMEDIA_RESOURCE_H__
-#define _ASM_TRIMEDIA_RESOURCE_H__
-
-#include <asm-generic/resource.h>
-
-#endif /* _ASM_TRIMEDIA_RESOURCE_H__ */
-
--- sysroots/pnx1500_ttisim_uclibc/sysroot/usr/include/asm/tlb.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ASM_TRIMEDIA_TLB_H__
-#define _ASM_TRIMEDIA_TLB_H__
-
-#define tlb_flush(tlb)	((void)0)
-
-#include <asm-generic/tlb.h>
-
-#endif /* _ASM_TRIMEDIA_TLB_H__ */
