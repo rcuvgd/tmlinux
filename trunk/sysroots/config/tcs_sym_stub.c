90251607c5a5cddda312d4d2a1adec200c804a54
camelguo <camelguo@c9ba4982-c440-0410-8649-23ceed688df0>
2009-03-06 03:35:27 +0000

    add stub symbol files for TCS

---
+++ trunk/sysroots/config/tcs_sym_stub.c
@@ -0,0 +1,34 @@
+int _dp;
+int _MMIO_base_init;
+
+int _float64_mul;
+int _float64_sub;
+int _uint32_to_float64;
+int _float64_to_uint32_round_to_zero;
+int _float64_div;
+int _float64_ge;
+int _float64_lt;
+int _float64_eq;
+int _float64_negate;
+int _float64_ne;
+int _float32_to_float64;
+int _int32_to_float64;
+int _float64_add;
+int _float64_to_float32;
+int _float64_gt;
+int _float64_to_int32_round_to_zero;
+
+int _rt_udiv;
+int _rt_umod;
+int _rt_mod;
+int _rt_idiv;
+int _rt_i64mod;
+int _rt_imod;
+int _rt_u64div;
+int _rt_u64mod;
+int _rt_i64div;
+
+/*The following symbol shoud be defined in uClibc*/
+int sqrt;
+int sbrk;
+int memcpy;
