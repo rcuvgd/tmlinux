c1feab5ce3ab587df374d3e585535e94c93f7d59
camelguo <camelguo@c9ba4982-c440-0410-8649-23ceed688df0>
2009-03-12 12:34:07 +0000

    validate stack pointer

--- issues/37/main.c
+++ issues/37/main.c
@@ -3,23 +3,25 @@
 #include <unistd.h>
 #include <errno.h>
 
+#include <ops/custom_ops.h>
+
+static unsigned long child_r4 = 0;
+static unsigned long father_r4 = 0;
+
 static pid_t
-/*run_child(unsigned char *colle, unsigned char *active, int quiet_mode)*/
 run_child(unsigned long colle, unsigned long active, int quiet_mode)
 {
     int cpid;
-
-    printf("run_child before vfork(%p,%p,%x)\n",colle,active,quiet_mode);
 
     if ((cpid = vfork()) < 0) {
 	return -1;
     } else if (cpid == 0) {
 	/* child */
-	printf("run_child child(%p,%p,%x)\n",colle,active,quiet_mode);
+	    child_r4 = RDREG(4);
 	exit(errno);
     }
     /* parent */
-    printf("run_child of parent (%p,%p,%x)\n",colle,active,quiet_mode);
+    father_r4 = RDREG(4);
 
     return cpid;
 }
@@ -28,6 +30,7 @@ int
 main(void)
 {
 	run_child((unsigned long)0x11111111,(unsigned long)0x22222222,0x33333333);
+	printf("child_r4 = %x, father_r4 = %x\n",child_r4, father_r4);
 	while(1);
 	return 0;
 }
