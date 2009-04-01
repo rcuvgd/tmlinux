3aa977b4605039df3e39eafa14c9e0242f386466
camelguo <camelguo@c9ba4982-c440-0410-8649-23ceed688df0>
2009-04-01 10:28:09 +0000

    Allocate memory dyanmically for kernel_groups

--- trunk/uClibc/libc/sysdeps/linux/common/getgroups.c
+++ trunk/uClibc/libc/sysdeps/linux/common/getgroups.c
@@ -25,10 +25,22 @@ int getgroups(int n, gid_t * groups)
 		return -1;
 	} else {
 		int i, ngids;
+
 #ifndef __TCS__
 		__kernel_gid_t kernel_groups[n = MIN(n, sysconf(_SC_NGROUPS_MAX))];
 #else
-		__kernel_gid_t kernel_groups[_SC_NGROUPS_MAX];
+		__kernel_gid_t* kernel_groups = NULL;
+
+		n = MIN(n, sysconf(_SC_NGROUPS_MAX));
+
+		if(n > 0) {
+			kernel_groups = malloc(sizeof(__kernel_gid_t)*n);
+			if(kernel_groups==NULL){
+				__set_errno(ENOMEM);
+				return -1;
+			}
+			memset(kernel_groups,0,sizeof(__kernel_gid_t)*n);
+		}
 #endif 
 
 		ngids = __syscall_getgroups(n, kernel_groups);
@@ -37,6 +49,11 @@ int getgroups(int n, gid_t * groups)
 				groups[i] = kernel_groups[i];
 			}
 		}
+
+#ifdef __TCS__
+		free(kernel_groups);
+#endif 
+
 		return ngids;
 	}
 }
